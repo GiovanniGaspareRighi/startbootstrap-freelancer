0357ab1c918a02a87bc91687a724b7e1e2fcb0ea
GiovanniGaspareRighi <andrea@192.168.0.101>
2018-02-06 11:43:46 +0100

    modifiche dario 06-feb

---
+++ dario/define.js
@@ -0,0 +1,75 @@
+// Variabili che definiscono il sito
+
+
+var candidato_nome = "Eulalia Grillo";
+var ruolo = "circoscrizione di Bologna";
+var slogan = "Per i molti non per i pochi";
+var candidato_img_url = "img/profile2.png";
+var facebook_link = "https://www.facebook.com/Eulalia-Grillo-159125864736835/";
+var twitter_link = "dewqeqweq";
+var linkedin_link = "";
+var wordpress_link = ""; //link alla pagina autore di Possibile o altro sito personale
+var youtube_link = "https://www.youtube.com/channel/UC6Bb0C_Ixcc-FS52Y0VBCqQ"; 
+
+// utility: https://word2cleanhtml.com
+// 
+var bio_short_url = "docs/bio_short.html";
+var bio_long_url = "docs/bio_long.html";
+
+var issues_url = "docs/my_issues.html";
+
+var why = "";
+
+// Viva Paremetri
+var viva_url = "http://testviva.possibile.com/";
+var donation_id = 4;
+var group_id = 376;
+
+
+// Viva Link
+var donation_url = "https://viva.possibile.com/backoffice/user_frm_donation.php?id_crowdfunding="+ donation_id;
+
+
+// Viva Mappa-Agenda ESEMPIO
+
+var markers = [
+    {
+        id: 19,
+        lat: 44.514003,
+        lng:11.259412,
+        id_type:'3',
+        type:'',
+        name:'Controvoglia',
+        url_facebook:'',
+        venue_name:'posto in mezzo alla campagna',
+        address:'Via Antonio Cavalieri Ducati, 100, Bologna, BO, Italia',
+        city:'Bologna',
+        province:'BO'
+    }, 
+    {
+        id: 8,
+        lat: 44.50367,
+        lng:11.312648,
+        id_type:'4',
+        type:'',
+        name:'storie di eventi',
+        url_facebook:'',
+        venue_name:'Casa del popolo Venti Pietre',
+        address:'Via Marzabotto, 2, Bologna, BO, Italia',
+        city:'Bologna',
+        province:'BO'
+    }, 
+    {
+        id: 1,
+        lat: 50.838694,
+        lng:4.375531,
+        id_type:'1',
+        type:'',
+        name:'presentazione Film',
+        url_facebook:'https://form.jotformeu.com/72473912391358',
+        venue_name:'Parlamento Europeo Bruxelles',
+        address:'European Parliament, Rue Wiertz, Bruxelles, Belgio',
+        city:'Bruxelles',
+        province:'--'
+    }
+]; 
